963304eaae795d361c154744ef25f684b5419321
Yura47 <rybakyra@gmail.com>
2021-11-25 12:15:09 +0200

    lab 3: task 15 commit

--- lab_3/main/views.py
+++ lab_3/main/views.py
@@ -7,5 +7,15 @@ def main(request):
     return render(request, 'main.html', {'parameter': "test"})
 
 def health(request):
-    response = {'date': 'test1', 'current_page': "test2", 'server_info': "test3", 'client_info': "test4"}
+    response = {
+    'date': datetime.now().strftime("Time: %H:%M:%S   Data: %Y/%M/%D"),
+    'current_page': request.get_host() + request.get_full_path(),
+    'server_info': "Name_OS: " + os.uname().sysname + ";   " +
+                   "Name_Node: " + os.uname().nodename + ";   " +
+                   "Release: " + os.uname().release + ";   " +
+                   "Version: " + os.uname().version + ";   " +
+                   "Indentificator:" + os.uname().machine,
+    'client_info': "Browser: " + request.META['HTTP_USER_AGENT'] + ";   " + "IP: " + request.META['REMOTE_ADDR']
+    }
     return JsonResponse(response)
+
